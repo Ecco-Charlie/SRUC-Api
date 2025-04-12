a657c4eb6e7c844315d6ac2c6b2e8b42cb388289
Rabano <rabano@soft-exe.org>
2025-04-12 17:14:56 -0600

    Start Server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"soft.exe/sruc/config"
+)
 
 func main() {
-	fmt.Println("Hola Mundo")
+	config.ShowBanner()
+	cfg := config.LoadApiConfig()
+	config.ConnectToDatabase(cfg)
+	router := config.NewRouter()
+
+	log.Println("El servidor inicio correctamente")
+	log.Printf("Escuchando en el puerto %s...", *cfg.Addr)
+	if err := http.ListenAndServe(*cfg.Addr, router.Mux); err != nil {
+		log.Fatalf("El servidor tuvo un error, causa: %s", err)
+	}
 }
